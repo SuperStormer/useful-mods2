9f5e8ce3013ccb7fcaeee8aba0754ae4f89f158d
Larry Xue <larry.p.xue@gmail.com>
2023-04-29 17:40:10 -0400

    side dropdown

--- web/index.js
+++ web/index.js
@@ -1,21 +1,28 @@
 (async function () {
 	let mods = await (await fetch("mods.json")).json();
+	let sides_map = {
+		all: ["Client", "Server", "Either", "Server Optional", "Client Optional", "Both"],
+		clientonly: ["Client", "Either", "Server Optional"],
+		serveronly: ["Server", "Either", "Client Optional"],
+	};
 	let version_dropdown = document.getElementById("version");
 	let modloader_dropdown = document.getElementById("modloader");
 	let type_dropdown = document.getElementById("type");
+	let side_dropdown = document.getElementById("side");
 	let results_body = document.getElementById("results-body");
 	let version_search = document.getElementById("advanced-version");
-	function update_params(modloader, version, type) {
-		history.pushState({}, "", "?" + new URLSearchParams({ modloader, version, type }));
+	function update_params(modloader, version, type, side) {
+		history.pushState({}, "", "?" + new URLSearchParams({ modloader, version, type, side }));
 	}
-	function update_table(modloader, version, type) {
+	function update_table(modloader, version, type, sides) {
 		results_body.innerHTML = "";
 
 		let results = mods
 			.filter(
 				(mod) =>
 					mod.versions.some((v) => v[0] === version && v[1] === modloader) &&
-					mod.type.includes(type)
+					mod.type.includes(type) &&
+					sides.includes(mod.side)
 			)
 			.sort((a, b) => a.name.localeCompare(b.name));
 
@@ -74,29 +81,39 @@
 		let modloader = modloader_dropdown.value;
 		let version = version_dropdown.value;
 		let type = type_dropdown.selectedOptions[0].value;
-		update_table(modloader, version, type);
-		update_params(modloader, version, type);
+		let side = side_dropdown.selectedOptions[0].value;
+		let sides = sides_map[side];
+
+		update_table(modloader, version, type, sides);
+		update_params(modloader, version, type, side);
 	}
 	function main() {
 		let params = new URLSearchParams(window.location.search);
 		let modloader = params.get("modloader") || "Forge";
 		let version = params.get("version") || "1.19.3";
 		let type = params.get("type") || "performance";
+		let side = params.get("side") || "all";
 		modloader_dropdown.value = modloader;
 		version_dropdown.value = version;
 		type_dropdown.value = type;
-		update_table(modloader, version, type);
+		side_dropdown.value = side;
+		let sides = sides_map[side] || side;
+		update_table(modloader, version, type, sides);
 
 		version_dropdown.addEventListener("change", on_dropdown_change);
 		modloader_dropdown.addEventListener("change", on_dropdown_change);
 		type_dropdown.addEventListener("change", on_dropdown_change);
+		side_dropdown.addEventListener("change", on_dropdown_change);
 		version_search.addEventListener("change", function () {
 			let modloader = modloader_dropdown.value;
 			let version = version_search.value;
 			let type = type_dropdown.selectedOptions[0].value;
+			let side = side_dropdown.selectedOptions[0].value;
+			let sides = sides_map[side];
+
 			version_dropdown.value = version;
-			update_table(modloader, version, type);
-			update_params(modloader, version, type);
+			update_table(modloader, version, type, sides);
+			update_params(modloader, version, type, side);
 		});
 	}
 	main();
